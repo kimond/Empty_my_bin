2503ce5b80037b7ddfb2eba2b033132623b32837
Kim Desrosiers <kimdesro@gmail.com>
2017-09-23 15:49:23 -0400

    Extract BinTile widget into its own file

--- lib/src/pages/HomePage.dart
+++ lib/src/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:empty_my_bin/src/Bin.dart';
+import 'package:empty_my_bin/src/widgets/BinTile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
@@ -78,25 +79,3 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-@override
-class BinTile extends StatelessWidget {
-  BinTile({this.snapshot, this.animation, this.onDelete});
-
-  final DataSnapshot snapshot;
-  final Animation animation;
-  final ValueChanged<String> onDelete;
-
-  void _handleDelete() {
-    onDelete(snapshot.key);
-  }
-
-  Widget build(BuildContext context) {
-    return new SizeTransition(
-        sizeFactor:
-        new CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-        child: new ListTile(
-          title: new Text(snapshot.value['name']),
-          trailing: new IconButton(icon: new Icon(Icons.delete, color: Colors.red,), onPressed: _handleDelete),
-        ));
-  }
-}
---
+++ lib/src/widgets/BinTile.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+@override
+class BinTile extends StatelessWidget {
+  BinTile({this.snapshot, this.animation, this.onDelete});
+
+  final DataSnapshot snapshot;
+  final Animation animation;
+  final ValueChanged<String> onDelete;
+
+  void _handleDelete() {
+    onDelete(snapshot.key);
+  }
+
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+        sizeFactor:
+        new CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+        child: new ListTile(
+          title: new Text(snapshot.value['name']),
+          trailing: new IconButton(icon: new Icon(Icons.delete, color: Colors.red,), onPressed: _handleDelete),
+        ));
+  }
+}
